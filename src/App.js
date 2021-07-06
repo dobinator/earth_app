87fcbfed57e4064408a038257d40efd0ed582f6b
grey cashmere <dobsonemily@gmail.com>
2021-07-06 15:06:24 -0700

    working on adding files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+
+import React, {useState} from "react";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+
+//pages
+
+import Nav from "./components/Nav";
+import LandingPg from "./pages/LandingPg";
+import SearchPage from "./pages/SearchPage";
+import Footer from "./components/Footer"; 
+
+import "tailwindcss/tailwind.css"; 
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <Router>
+    <div>
+    <Nav/>
+
+    <Footer/>
     </div>
+    // </Router>
   );
 }
 
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,24 +5,24 @@ function Nav() {
 
   return (
     // left side
-   <div className="bg-blue-700  p-4 flex justify-between items-center">
+   <div className="bg-bg-blue-300 p-4 flex justify-between items-center">
      
      <div className="flex items-center">
      <img 
-      src="/images/icon.png"
+      src= "./images/icons.png"
       width= "50"
       alt="graphic of building with telescope"
       className="mr-2"></img>
-      <a href= "#" className="inline-block p-2 text-blue-100 hover:text-blue-50 mr-2">Home</a>
-      <a href= "#" className="inline-block p-2 text-blue-100 hover:text-blue-50">Search</a>
+      <a href= "#" className="inline-block p-2 text-blue-600 hover:text-blue-200 mr-2">Home</a>
+      <a href= "#" className="inline-block p-2 text-blue-600 hover:text-blue-200">Search</a>
       </div>  
     
     {/* <div className="hidden md:block"> */}
     <div>
       <a href= "#"
-      className= "inline-block p-2 text-blue-100 mr-2">Login</a>
+      className= "inline-block p-2 text-blue-600 mr-2">Login</a>
       <a href= "#"
-      className= "inline-block p-2 px-4 text-indigo-100 bk-indigo-500 hover: bg-indigo 400 hover:text- indigo 50 rounded">Sign up</a>
+      className= "inline-block p-2 px-4 text-yellow-600 bk-yellow-400 hover: bg-indigo 600 hover:text- indigo 50 rounded">Sign up</a>
     </div>
 
 
