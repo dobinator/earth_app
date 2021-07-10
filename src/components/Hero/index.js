b468d85d255a090cc2929b1d168a7e1c28669971
grey cashmere <dobsonemily@gmail.com>
2021-07-09 18:54:55 -0700

    adding files and updates for basic animation

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Typical from 'react-typical'; 
 
 
 function Hero(){
@@ -6,7 +7,11 @@ return (
     <div className="md:flex justify-between py-20 px-10 text-blue-100" style ={{backgroundColor: "#a1caf1"}}>
         <div className= "md:w-1/2 mb-10 md:mb-0">
             <h2 className="text-2xl md:text-4xl lg:text-6xl mb-6"style= {{color: "#002387"}}>Welcome to Earth </h2>
-            <p className= "mb-6 py-10" style= {{color: "#002387"}}>Let's learn about our wonderful planet</p>
+            <Typical
+           steps= {[]}
+           loop={Infinity}
+           wrapper="p"
+            />
             <a href="#" className="inline-block py-3 px-6 text-lg bg-rey-400 text-grey-800 rounded mr-2">Learn More</a>
             <a href="#"className="inline-block py-3 px-6 text-lg bg-rey-400 text-grey-800 rounded mr-2">Get Started</a>
      </div>
@@ -25,3 +30,13 @@ return (
 
 
 export default Hero; 
+
+
+
+// Earth is the third planet from the sun in our solar system.
+// Earth has been calculated to be around 4.5 billion years old!
+// Earth orbits around the sun around 30 kilometers per second.
+// Earth both zooms through space and its also spins on its axis!
+// Earth has a diameter that measures 12,800 kilometers.
+// Earth is the only planet in our solar system known to support life.
+// Earth has an atmosphere that is "blanket" of gases, mainly oxygen and nitrogen.
